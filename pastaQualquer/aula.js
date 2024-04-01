44355a9ab7093e19940a23d3f798c32ce58a96a1
Leonardo Andriolo <159090875+LeonardoAndriolo@users.noreply.github.com>
2024-04-01 17:47:00 -0300

    Testa conflict

--- pastaQualquer/aula.js
+++ pastaQualquer/aula.js
@@ -1,5 +1,5 @@
 var quantidadeBTC = 5894.9
 var valor = quantidadeBTC*69000
-console.log(`Você tem ${quantidadeBTC} bitcoins, ou seja, ${valor} dólares.`)
+console.log(`Você tem ${quantidadeBTC} bitcoins
 valor = quantidadeBTC*600000
 console.log(`Houve uma nova cotação do btc, que agora está valendo $${valor/quantidadeBTC}, ou seja, com ${quantidadeBTC} você tem $${valor} agora. ;-;`)
